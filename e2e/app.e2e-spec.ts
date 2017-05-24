4c1bd922d61da61930163e6ce363e48d61dbbae1
MadDeveloper <sergent.julien@icloud.com>
2017-05-24 12:32:12 +0200

    feat(app): Scaffold app

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SnipzPage } from './app.po';
+
+describe('snipz App', () => {
+  let page: SnipzPage;
+
+  beforeEach(() => {
+    page = new SnipzPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
