4bdab2eee5dd4853a7a4088aaab6484c8ba239da
Quentin <quentinpomarel@gmail.com>
2017-05-25 16:32:22 +0200

    feat(app) : add app-routing and update app.component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent }
+];
+
+export default routes;
