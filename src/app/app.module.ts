b419816aff0e15d4d4bdc6379eef9e27385824f7
Quentin <quentinpomarel@gmail.com>
2017-05-25 16:07:48 +0200

    Add home and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
