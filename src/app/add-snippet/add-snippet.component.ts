40b206e603d1b1a5d89cf42e380cdd45ae8605d6
Quentin <quentinpomarel@gmail.com>
2017-06-03 12:06:29 +0200

    feat(add-snippet): add codemirror editor, add control to add dynamicaly code block

--- src/app/add-snippet/add-snippet.component.ts
+++ src/app/add-snippet/add-snippet.component.ts
@@ -1,38 +1,67 @@
 import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-add-snippet',
-  templateUrl: './add-snippet.component.html',
-  styleUrls: ['./add-snippet.component.scss']
+    selector: 'app-add-snippet',
+    templateUrl: './add-snippet.component.html',
+    styleUrls: ['./add-snippet.component.scss']
 })
 export class AddSnippetComponent implements OnInit {
 
-    rowData = [
-        {
-            language: 'JS',
-            code: 'js1'
-        }
+    rowData = []
+    language = [
+        { id: 1, name: 'JS', value: 'javascript' },
+        { id: 2, name: 'C#', value: 'clike' },
+        { id: 3, name: 'PHP', value: 'php' },
+        { id: 4, name: 'Java', value: 'clike' }
     ]
-
     constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.rowData.push({
+            code: '',
+            language: null,
+            selectedValue: null,
+            codemirrorConfig: {
+                lineNumbers: true,
+                smartIndent: true,
+                mode: {
+                    name: 'javascript',
+                    typescript: true
+                },
+                theme: 'dracula'
+            }
+        })
+    }
 
-  addCodeBlock()
-  {
-    this.rowData.push({
-        language: 'JS',
-        code: ''
-    })
-  }
+    addCodeBlock() {
+        this.rowData.push({
+            language: null,
+            code: '',
+            selectedValue: null,
+            codemirrorConfig: {
+                lineNumbers: true,
+                smartIndent: true,
+                mode: {
+                    name: 'javascript',
+                    typescript: true
+                },
+                theme: 'dracula'
+            }
+        })
+    }
 
-  removeCodeBlock(row)
-  {
-       const index: number = this.rowData.indexOf(row)
+    removeCodeBlock(row) {
+        const index: number = this.rowData.indexOf(row)
         if (index !== -1) {
             this.rowData.splice(index, 1)
         }
-  }
+    }
+
+    onChangeLanguage(i) {
+        console.log(this.rowData[i].language)
+        this.rowData[i].codemirrorConfig.mode.name = this.rowData[i].selectedValue.value
+        this.rowData[i].language = this.rowData[i].selectedValue.name
+        console.log(this.rowData[i].language)
+    }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router'
 
-import { HomeComponent } from './home/home.component'
-import { ConnectComponent } from './connect/connect.component'
-import { SignupComponent } from './signup/signup.component'
+import {  HomeComponent } from './home/home.component'
+import {  ConnectComponent } from './connect/connect.component'
+import {  SignupComponent } from './signup/signup.component'
 import { SnippetDetailsComponent } from './snippet-details/snippet-details.component'
 import { AddSnippetComponent } from './add-snippet/add-snippet.component'
 import { ProfileComponent } from './profile/profile.component'
@@ -10,10 +10,10 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'snippet/add', component: AddSnippetComponent },
     { path: 'snippet/:id', component: SnippetDetailsComponent },
-    { path: 'add', component: AddSnippetComponent },
-    { path: 'connect', component:  ConnectComponent },
-    { path: 'signUp', component:  SignupComponent },
+    { path: 'connect', component: ConnectComponent },
+    { path: 'signUp', component: SignupComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'profile/edit', component: EditProfileComponent }
 ]
