ca9bf6d98a8e7f23e0b8941f9ae03af639970910
MadDeveloper <sergent.julien@icloud.com>
2017-06-05 18:34:30 +0200

    fix(edit-snippet): conflicts

--- src/app/edit-snippet/edit-snippet.component.ts
+++ src/app/edit-snippet/edit-snippet.component.ts
@@ -1,33 +1,22 @@
 import { Component, OnInit } from '@angular/core'
 
 @Component({
-<<<<<<< HEAD
-    selector: 'app-add-snippet',
-=======
     selector: 'app-edit-snippet',
->>>>>>> feat(many): design, renaming, etc.
     templateUrl: './edit-snippet.component.html',
     styleUrls: ['./edit-snippet.component.scss']
 })
 export class EditSnippetComponent implements OnInit {
-<<<<<<< HEAD
-=======
     private id: number
     private name: string
     private description: string
     private codeBlocks: Array<any>
->>>>>>> feat(many): design, renaming, etc.
 
     constructor() { }
 
     ngOnInit() {
-<<<<<<< HEAD
-
-=======
         this.name = ''
         this.description = ''
         this.id = 1
->>>>>>> feat(many): design, renaming, etc.
     }
 
 
