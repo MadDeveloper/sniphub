90931bad4bb0d4bf3d1b360cf47a831e70f72348
Quentin <quentinpomarel@gmail.com>
2017-06-11 13:09:03 +0200

    Revert "feat(404): add 404 page and redirect route"
    This reverts commit ecd632c6c5f01a934c3a037f6d86e73ef53f346d.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,46 +9,43 @@ import { SnippetsRequestsComponent } from './snippets-requests/snippets-requests
 import { EditSnippetComponent } from './edit-snippet/edit-snippet.component'
 import { ProfileComponent } from './profile/profile.component'
 import { EditProfileComponent } from './edit-profile/edit-profile.component'
-import  { NotificationsComponent }  from './notifications/notifications.component'
+import { NotificationsComponent } from './notifications/notifications.component'
 import { ContactComponent } from './contact/contact.component'
-import { SearchComponent }  from './search/search.component'
-import { NotFoundComponent }  from './not-found/not-found.component'
+import { SearchComponent } from './search/search.component'
 import { AuthenticationGuard } from 'app/guards/authentication'
 import { SnippetResolverGuard } from './guards/snippet/index'
 import { UserResolverGuard } from './guards/user/index'
-import { SnippetsRequestsResolverGuard }  from './guards/snippet/snippets-requests-resolver.guard'
+import { SnippetsRequestsResolverGuard } from './guards/snippet/snippets-requests-resolver.guard'
 import { SnippetRequestResolverGuard } from './guards/snippet/snippet-request-resolver.guard'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'search', component: SearchComponent },
-    { path: 'snippet/new', component: EditSnippetComponent, canActivate: [AuthenticationGuard] },
+    { path: 'snippet/new', component: EditSnippetComponent, canActivate: [ AuthenticationGuard ] },
     {
         path: 'snippet/edit/:id',
         component: EditSnippetComponent,
-        canActivate: [AuthenticationGuard],
-        resolve: [SnippetResolverGuard]
+        canActivate: [ AuthenticationGuard ],
+        resolve: [ SnippetResolverGuard ]
     },
     {
         path: 'snippets/requests',
         component: SnippetsRequestsComponent,
-        canActivate: [AuthenticationGuard],
-        resolve: [SnippetsRequestsResolverGuard]
+        canActivate: [ AuthenticationGuard ],
+        resolve: [ SnippetsRequestsResolverGuard ]
     },
     {
         path: 'snippets/requests/:id',
         component: SnippetRequestComponent,
-        canActivate: [AuthenticationGuard],
-        resolve: [SnippetRequestResolverGuard]
+        canActivate: [ AuthenticationGuard ],
+        resolve: [ SnippetRequestResolverGuard ]
     },
-    { path: 'snippet/:id', component: SnippetDetailsComponent, resolve: [SnippetResolverGuard] },
+    { path: 'snippet/:id', component: SnippetDetailsComponent, resolve: [ SnippetResolverGuard ] },
     { path: 'signin', component: ConnectComponent },
     { path: 'signup', component: SignupComponent },
-    { path: 'profile', component: ProfileComponent, canActivate: [AuthenticationGuard] },
-    { path: 'profile/edit', component: EditProfileComponent, canActivate: [AuthenticationGuard] },
-    { path: 'profile/:id', component: ProfileComponent, resolve: [UserResolverGuard] },
+    { path: 'profile', component: ProfileComponent, canActivate: [ AuthenticationGuard ] },
+    { path: 'profile/edit', component: EditProfileComponent, canActivate: [ AuthenticationGuard ] },
+    { path: 'profile/:id', component: ProfileComponent, resolve: [ UserResolverGuard ] },
     { path: 'contact', component: ContactComponent },
-    { path: 'notifications', component: NotificationsComponent, canActivate: [AuthenticationGuard] },
-    { path: 'notfound', component: NotFoundComponent },
-    { path: '**', redirectTo: '/notfound' }
+    { path: 'notifications', component: NotificationsComponent, canActivate: [ AuthenticationGuard ] }
 ]
