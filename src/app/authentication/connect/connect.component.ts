690afdf87b0f86f975cf476edf1d39d350a9474e
TheApple <quentinpomarel@gmail.com>
2017-06-27 13:01:00 +0200

    feat(authent) : implement firebase authent with git, facebook and google

--- src/app/authentication/connect/connect.component.ts
+++ src/app/authentication/connect/connect.component.ts
@@ -3,17 +3,26 @@ import { Router } from '@angular/router'
 import { User } from '../../core/interfaces/user/user'
 import { AuthenticationService } from '../services/authentication.service'
 
+import { AngularFireAuth } from 'angularfire2/auth'
+import { Observable } from 'rxjs/Observable'
+
 @Component({
-  selector: 'app-connect',
-  templateUrl: './connect.component.html',
-  styleUrls: ['./connect.component.scss']
+    selector: 'app-connect',
+    templateUrl: './connect.component.html',
+    styleUrls: ['./connect.component.scss']
 })
 export class ConnectComponent implements OnInit {
 
+
     constructor(
         private authentication: AuthenticationService,
-        private router: Router) { }
+        private router: Router,
+        private afAuth: AngularFireAuth) {
 
+         }
+
+
+    // let provider = new firebase.auth.GoogleAuthProvider()
     ngOnInit() { }
 
     login() {
@@ -25,6 +34,18 @@ export class ConnectComponent implements OnInit {
         }
 
         this.authentication.login(user)
+    }
+
+    googleLogin() {
+        this.authentication.loginGoogle()
+    }
+
+    githubLogin() {
+        this.authentication.loginGitHub()
+    }
+
+    facebookLogin() {
+        this.authentication.loginFacebook()
     }
 
 }
--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@angular/core'
 import { Router } from '@angular/router'
 import { User } from '../../core/interfaces/user/user'
+import { Observable } from 'rxjs/Observable'
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as firebase from 'firebase/app'
+import { AngularFireDatabase } from 'angularfire2/database'
+import { UserService } from '../../core/services/user/user.service'
 
 @Injectable()
 export class AuthenticationService {
     redirectUrl: string
+    userFirebase: Observable<firebase.User>
+    user: User
 
-    constructor(private router: Router) { }
+    constructor(
+        private router: Router,
+        private afAuth: AngularFireAuth,
+        private userService: UserService) {
+        this.userFirebase = afAuth.authState
+
+        this.userFirebase.subscribe(userFirebase => {
+            console.log(userFirebase)
+            if (userFirebase) {
+
+                this.userService
+                    .find(userFirebase.uid)
+                    .map(user => {
+                        this.user = user
+                        console.log(user.email)
+                    })
+
+
+            }
+        })
+    }
 
     isAuthenticated() {
         return !!this.user
@@ -19,8 +46,20 @@ export class AuthenticationService {
     login(user: User) {
         const url = this.redirectUrl || '/'
 
-        this.user = user
+        // this.user = user
         this.router.navigate([url])
+    }
+
+    loginGoogle() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    }
+
+    loginGitHub() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider())
+    }
+
+    loginFacebook() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
     }
 
     logout() {
@@ -28,16 +67,16 @@ export class AuthenticationService {
         this.router.navigate(['/'])
     }
 
-    set user(user: User) {
-        localStorage.setItem('user', JSON.stringify(user))
-    }
+    // set user(user: User) {
+    //     localStorage.setItem('user', JSON.stringify(user))
+    // }
 
-    get user(): User {
-        try {
-            return JSON.parse(localStorage.getItem('user'))
-        } catch (e) {
-            return null
-        }
-    }
+    // get user(): User {
+    //     try {
+    //         return JSON.parse(localStorage.getItem('user'))
+    //     } catch (e) {
+    //         return null
+    //     }
+    // }
 
 }
